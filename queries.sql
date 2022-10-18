eec8c67e723aeb2869472b81769f55fcc6f6b240
samiullah997 <samiullahk997@gmail.com>
2022-10-18 17:24:37 +0500

    update queries.sql file to add all queries

--- queries.sql
+++ queries.sql
@@ -1,10 +1,19 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name LIKE '%mon';
-SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
-SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
-SELECT date_of_birth FROM animals WHERE name = 'Agumon' or name = 'Pikachu';
-SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
-SELECT * FROM animals WHERE neutered = true;
-SELECT * FROM animals WHERE name != 'Gabumon';
-SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species != 'digimon';
+BEGIN TRANSACTION;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT DELET_ONE;
+UPDATE animals set weight_kg = (weight_kg*-1);
+ROLLBACK TO DELET_ONE;
+UPDATE animals set weight_kg = (weight_kg*-1);
+COMMIT;
+SELECT * FROM animals ORDER BY id;
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts <= 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT MAX(escape_attempts) FROM animals;
+SELECT MAX(weight_kg) FROM animals;
+SELECT MIN(weight_kg) FROM animals;
+SELECT AVG(escape_attempts) FROM animals WHERE date_of_birth  BETWEEN '1990-01-01' AND '2000-12-31';
